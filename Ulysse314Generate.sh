50dcb2a7956b5c2f4940993501d36f7aab6f045d
ulysse314 <ulysse314>
2020-09-16 23:05:57 +0200

    Adding c version

--- Ulysse314Generate.sh
+++ Ulysse314Generate.sh
@@ -2,3 +2,4 @@
 
 python3 -m pymavlink.tools.mavgen --lang=ObjC --wire-protocol=2.0 --output=MavlinkGenerated/objc message_definitions/v1.0/common.xml
 python3 -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output=MavlinkGenerated/mavlink message_definitions/v1.0/common.xml
+python3 -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output=MavlinkGenerated/c message_definitions/v1.0/common.xml
