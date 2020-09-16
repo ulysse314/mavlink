c21c0e16dc4dca7de09da0dc73ce4bdd1f72ecee
ulysse314 <ulysse314>
2020-09-16 22:39:06 +0200

    Adding generated ulysse314 subproject

---
+++ Ulysse314Generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 -m pymavlink.tools.mavgen --lang=ObjC --wire-protocol=2.0 --output=MavlinkGenerated/objc message_definitions/v1.0/common.xml     
+python3 -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output=MavlinkGenerated/mavlink message_definitions/v1.0/common.xml
